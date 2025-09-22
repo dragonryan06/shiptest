56176f596ef7ab81b6f6d79d15a16dc4412d4b53
dragonryan06 <dragonryan06@gmail.com>
2025-09-21 22:00:44 -0400

    Populate inventory with parts

--- Editor/Editor.cs
+++ Editor/Editor.cs
@@ -5,6 +5,8 @@ namespace ShipTest.Editor;
 
 public partial class Editor : Node2D
 {
+    private const string InventoryGridPath =
+        "HUD/PartInventory/PanelContainer/VBoxContainer/ScrollContainer/MarginContainer/GridContainer";
     private readonly Array _parts;
 
     public Editor()
@@ -18,6 +20,24 @@ public partial class Editor : Node2D
             var jsonString = FileAccess.Open("res://Resources/Scenes/Editor/parts.json", 
                 FileAccess.ModeFlags.Read).GetAsText();
             return Json.ParseString(jsonString).As<Array>();
+        }
+    }
+
+    public override void _Ready()
+    {
+        var partInventoryItem = GD.Load<PackedScene>("res://Resources/Scenes/Editor/part_inventory_item.tscn");
+        var grid = GetNode<GridContainer>(InventoryGridPath);
+        
+        foreach (var p in _parts)
+        {
+            var part = p.AsGodotDictionary();
+            var inventoryItem = partInventoryItem.Instantiate<Button>();
+            
+            inventoryItem.GetNode<Label>("Label").Text = part["name"].AsString();
+            inventoryItem.Icon = GD.Load<CompressedTexture2D>(part["icon"].AsString());
+            inventoryItem.SetMeta("part_id", part["id"].AsInt32());
+            
+            grid.AddChild(inventoryItem);
         }
     }
 }
--- Resources/Scripts/editor_hud.gd
+++ Resources/Scripts/editor_hud.gd
@@ -2,17 +2,19 @@ extends CanvasLayer
 
 const INVENTORY_BACKSTAGE = Vector2(1152.0, 0.0)
 
-signal selection_changed(hotbar_idx)
+# part_id is the order of the part in parts.json
+signal selection_changed(part_id: int)
 
 func _on_hotbar_button_pressed(idx: int) -> void:
-	selection_changed.emit(idx)
+	pass
+	#selection_changed.emit(part_id)
 
 func _on_show_inventory_toggled(toggled_on: bool) -> void:
 	var tween = get_tree().create_tween()
 	tween.set_trans(Tween.TRANS_SINE)
 	if toggled_on:
-		$HUD/ShowInventory.text = "Hide Inventory"
-		tween.tween_property($HUD/PartInventory, "position", Vector2.ZERO, 0.25)
+		$ShowInventory.text = "Hide Inventory"
+		tween.tween_property($PartInventory, "position", Vector2.ZERO, 0.25)
 	else:
-		$HUD/ShowInventory.text = "Show Inventory"
-		tween.tween_property($HUD/PartInventory, "position", INVENTORY_BACKSTAGE, 0.25)
+		$ShowInventory.text = "Show Inventory"
+		tween.tween_property($PartInventory, "position", INVENTORY_BACKSTAGE, 0.25)
